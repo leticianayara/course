e0efdf747e86ad5db736195d2309a8209faf6797
Leticia Nayara de LIma Costa <leticianayara@gmail.com>
2025-02-18 16:40:11 -0300

    excepttion handling - delete

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exception.DatabaseException;
 import com.example.course.services.exception.ResourceNotFoundException;
 
 @Service
@@ -15,34 +18,42 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
-	public void delete (Long id) {
-		repository.deleteById(id);
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		} catch (RuntimeException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
-	public User update(Long id , User obj) {
+
+	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(obj);
 	}
-	
+
 	private void updateData(User now, User others) {
 		now.setName(others.getName());
 		now.setEmail(others.getEmail());
 		now.setPhone(others.getPhone());
 	}
-	
+
 }
